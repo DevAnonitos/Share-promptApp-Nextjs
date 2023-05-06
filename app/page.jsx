988b840d48b7b73357b0f3fa901247b5baf43911
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-06 21:31:03 +0700

    update_page&layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 
 const page = () => {
+
+    const [showFeed, setShowFeed] = useState(false);
+
+    useEffect(() => {
+        setShowFeed(true);
+    }, [])
+
     return (
-        <div>
-            App
-        </div>
+        <>
+            <section className='w-full flex-center flex-col'>
+                <h1 className='head_text text-center'>
+                    Discover & Share
+                    <br className='max-md:hidden' />
+                    <span className='orange_gradient text-center'>
+                        AI-Prompts GPT
+                    </span>
+                </h1>
+
+                <p className='desc text-center'>
+                    Promptopia is an open-source AI prompting tool for modern world to
+                    discover, create and share creative prompts
+                </p>
+
+                <div className={`feed ${showFeed ? 'feed--show' : ''}`}>
+                    {/* <Feed /> */}
+                    Hello
+                </div>
+            </section>
+        </>
     );
 };
 
