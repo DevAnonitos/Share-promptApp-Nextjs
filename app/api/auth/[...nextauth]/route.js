ba0b54766e4c9754b147ba3f8fed24cd1b380749
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-08 21:46:20 +0700

    update-Auth/user&NextConfig

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@/utils/database";
+import User from "@/models/user";
 
 const handler = NextAuth({
     providers: [
@@ -11,12 +12,33 @@ const handler = NextAuth({
     ],
     callbacks: {
         async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email,
+            })
 
+            session.user.id = sessionUser._id.toString();
+
+            sessionStorage.setItem('user', JSON.stringify(session.user));
+            
+            return session;
         },
         async signIn({ account, profile, user, credentials }) {
             try {
                 await connectToDB();
-                
+
+                const userExists = await User.findOne({
+                    email: profile.email,
+                });
+
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
+
+                return true;
             } catch (error) {
                 console.log("Error checking if user exists: ", error.message);
                 return false;
