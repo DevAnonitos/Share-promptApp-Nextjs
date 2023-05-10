a412182c4845baa64943501f507fc9677718439c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-10 16:38:38 +0700

    update-Feed&FixError

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { Feed } from '@/components';
+import Feed from "../components/Feed";
 
 const page = () => {
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+"use-client";
+
+import React, { useState, useEffect } from 'react';
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div>
+
+        </div>
+    )
+};
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState("");
+
+    const handleSearchChange = (e) => {
+
+    };
+
     return (
-        <div>Feed</div>
+        <section className='feed'>
+            <form className='relative w-full flex-center'>
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username...'
+                    value={searchText}
+                    required
+                    onChange={handleSearchChange}
+                    className='search_input peer'
+                />
+            </form>
+        </section>
     );
 };
 
