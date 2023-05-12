9cb637318b89909a4783d5f7b79c1ca79c36c054
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-12 22:34:47 +0700

    update-addLoader/CreatePrompt

---
+++ app/create-prompt/loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Loading = () => {
+    return (
+        <div className='w-full flex-center'>
+            <Image
+                src='assets/icons/loader.svg'
+                width={50}
+                height={50}
+                alt='loader'
+                className='object-contain'
+            />
+        </div>
+    );
+};
+
+export default Loading;
