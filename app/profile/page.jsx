57805f646af2d64b773093c54b6dc509317e2491
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-12 16:53:04 +0700

    update-AddMyProfilepage

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+import { Profile } from '@/components';
 
 const MyProfile = () => {
+
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    const [myPosts, setMyPosts] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
+    };
+
+    const handleDelete = async (post) => {
+
+    };
+
     return (
-        <div>MyProfile</div>
+        <div>
+            <Profile
+                name='My'
+                desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+                data={myPosts}
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+            />
+        </div>
     );
 };
 
