0174cdc04f3c074b23b16e3e4c8907d47406b026
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-13 10:00:09 +0700

    update-Add/Profile&PromptCardProfile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -9,6 +9,20 @@ const Profile = ({ name, desc, data , handleEdit, handleDelete }) => {
                     {name} Profile
                 </span>
             </h1>
+            <p className='desc text-left'>
+                {desc}
+            </p>
+
+            <div className='mt-10 prompt_layout'>
+                {data.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleEdit={() => handleEdit && handleEdit(post)}
+                        handleDelete={() => handleDelete && handleDelete(post)}
+                    />
+                ))}
+            </div>
         </section>
     );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -32,7 +32,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
     };
 
     return (
-        <div className='prompt_card shadow-lg'>
+        <div className='prompt_card shadow-lg hover:shadow-xl'>
             <div className='flex justify-between items-start gap-5'>
                 <div
                     className='flex-1 flex justify-start
@@ -82,6 +82,23 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             >
                 #{post.tag}
             </p>
+
+            {session?.user.id === post.creator._id && pathName === "/profile" && (
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p
+                        className='font-inter text-sm green_gradient cursor-pointer'
+                        onClick={handleEdit}
+                    >
+                        Edit
+                    </p>
+                    <p
+                        className='font-inter text-sm orange_gradient cursor-pointer'
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
