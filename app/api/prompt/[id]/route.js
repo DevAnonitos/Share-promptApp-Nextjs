d56b02705767ccbdbc34e00664dec9eaee82c280
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-15 09:41:17 +0700

    update-Prompt-Id/update-prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -3,51 +3,40 @@ import { connectToDB } from "../../../../utils/database";
 
 export const GET = async (request, { params }) => {
     try {
-        await connectToDB();
+        await connectToDB()
 
-        const prompt = await Prompt.findById(params.id).populate("creator");
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        if (!prompt) return new Response("Prompt Not Found", { status: 404 });
 
-        if(!prompt) {
-            return new Response("Prompt not found", {
-                status: 404,
-            });
-        }
+        return new Response(JSON.stringify(prompt), { status: 200 })
 
-        return new Response(JSON.stringify(prompt), {
-            status: 200,
-        });
     } catch (error) {
-        return new Response("Internal Server Error", {
-            status: 500,
-        });
+        return new Response("Internal Server Error", { status: 500 });
     }
-};
+}
 
-export const PATH = async (request, { params }) => {
+export const PATCH = async (request, { params }) => {
     const { prompt, tag } = await request.json();
 
     try {
         await connectToDB();
 
+        // Find the existing prompt by ID
         const existingPrompt = await Prompt.findById(params.id);
 
-        if(!existingPrompt) {
-            return new Response("Prompt not founded", {
-                status: 404,
-            });
+        if (!existingPrompt) {
+            return new Response("Prompt not found", { status: 404 });
         }
 
+        // Update the prompt with new data
         existingPrompt.prompt = prompt;
         existingPrompt.tag = tag;
 
-        return new Response("Successfully update-prompt!", {
-            status: 200,
-        });
+        await existingPrompt.save();
 
+        return new Response("Successfully updated the Prompts", { status: 200 });
     } catch (error) {
-        return new Response("Error updating Prompt", {
-            status: 500,
-        });
+        return new Response("Error Updating Prompt", { status: 500 });
     }
 };
 
@@ -55,14 +44,11 @@ export const DELETE = async (request, { params }) => {
     try {
         await connectToDB();
 
+        // Find the prompt by ID and remove it
         await Prompt.findByIdAndRemove(params.id);
 
-        return new Response("Prompt delete successfully", {
-            status: 200,
-        });
+        return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
-        return new Response("Error deleting prompt", {
-            status: 500,
-        });
+        return new Response("Error deleting prompt", { status: 500 });
     }
 };
---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Form from "../../components/Form";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const UpdatePrompt = () => {
+
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get("id");
+
+    const [post, setPost] = useState({
+        prompt: "",
+        tag: "",
+    });
+    const [submitting, setSubmitting] = useState(false);
+
+    useEffect(() => {
+        const getPromptDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = await response.json();
+
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag,
+            });
+        };
+
+        if(promptId) getPromptDetails();
+    },[promptId]);
+
+    const updatePrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+
+        if(!promptId) {
+            return toast.error("Missing PromptId!");
+        };
+
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag,
+                })
+            });
+
+            if(response.ok) {
+                toast.success("Update Prompt successfully");
+                router.push("/");
+            } else {
+                toast.error("Update Prompt failed");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("SomeThing went wrong!");
+        } finally {
+            setSubmitting(false);
+        }
+    }
+    return (
+        <div>
+            <Form
+                type='Edit'
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </div>
+    );
+};
+
+export default UpdatePrompt;
