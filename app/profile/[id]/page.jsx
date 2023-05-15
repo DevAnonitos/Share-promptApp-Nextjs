0603e74436d99f491765fbadb7d406736e937fe7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-15 10:13:21 +0700

    update-Add_profile

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+import Profile from "../../../components/Profile";
+
+
+const UserProfile = ({ params }) => {
+    const searchParams = useSearchParams();
+    const userName = searchParams.get("username");
+
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${params?.id}/posts`);
+            const data = await response.json();
+
+            setUserPosts(data);
+        };
+
+        if (params?.id) fetchPosts();
+    }, [params.id]);
+
+    return (
+        <Profile
+            name={userName}
+            desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+            data={userPosts}
+        />
+    );
+};
+
+export default UserProfile;
