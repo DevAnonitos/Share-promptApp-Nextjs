da2ed6b68e46f3931d28174bbc49563b5bc87108
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-03-19 17:31:36 +0700

    update:Add->FixStyle

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const page = () => {
     return (
         <>
             <section className='w-full flex-center flex-col'>
-                <h1 className='head_text text-center'>
+                <h1 className='head_text text-center '>
                     Discover & Share
                     <br className='max-md:hidden' />
                     <span className='orange_gradient text-center'>
@@ -24,7 +24,7 @@ const page = () => {
 
                 <p className='desc text-center'>
                     PromptApp is an open-source AI prompting tool for modern world to
-                    discover, create and share creative prompts
+                    discover, create and share creative prompts.
                 </p>
                 
                 <Feed />
