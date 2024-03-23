259e382a2ffb7095ca36a9c3bcd8212b71cb95c9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-03-24 00:13:04 +0700

    update:Add->LoadingTestComponents

--- app/loadingLoading.cy.jsx
+++ app/loadingLoading.cy.jsx
@@ -4,6 +4,12 @@ import Loading from './loading'
 describe('<Loading />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Loading />)
+    cy.mount(<Loading />);
+    cy.get('.w-full.flex-center').should('exist');
+    cy.get('img')
+      .should('have.attr', 'src', 'assets/icons/loader.svg')
+      .and('have.attr', 'alt', 'loader')
+      .and('have.attr', 'width', '50')
+      .and('have.attr', 'height', '50');
   })
 })
