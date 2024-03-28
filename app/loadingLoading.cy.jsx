aedefe949404567d37f959b623dd001f68e1b923
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-03-28 21:36:12 +0700

    update:Add->TestLoadingComponents

--- app/loadingLoading.cy.jsx
+++ app/loadingLoading.cy.jsx
@@ -3,13 +3,21 @@ import Loading from './loading'
 
 describe('<Loading />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Loading />);
-    cy.get('.w-full.flex-center').should('exist');
-    cy.get('img')
-      .should('have.attr', 'src', 'assets/icons/loader.svg')
-      .and('have.attr', 'alt', 'loader')
-      .and('have.attr', 'width', '50')
-      .and('have.attr', 'height', '50');
+    cy.readFile('public/assets/icons/loader.svg', { encoding: 'base64' }).then((svg) => {
+      cy.intercept('_next/image*', (req) => {
+        req.reply({
+          statusCode: 200,
+          headers: { 'Content-Type': 'image/svg+xml/png' },
+          body: img.buffer,
+        });
+      });
+      cy.mount(<Loading />);
+      cy.get('.w-full.flex-center').should('exist');
+      cy.get('img')
+        .should('have.attr', 'src', 'assets/icons/loader.svg')
+        .and('have.attr', 'alt', 'loader')
+        .and('have.attr', 'width', '50')
+        .and('have.attr', 'height', '50');
+    });
   })
 })
