674df6822f96903e3ee52508b52b6fd42e35bed7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-13 22:58:10 +0700

    update:Add->TestComponentsInHomePage

--- tests/pages/Home.test.jsx
+++ tests/pages/Home.test.jsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import HomePage from "@/app/page";
 
+
+jest.mock('@/components/Feed', () => ({
+  __esModule: true,
+  default: () => <div data-testid="feed-mock">Mocked Feed</div>,
+}));
+
 describe('HomePage', () => {
   
   it("should render a HomePage text", () => {
@@ -9,5 +15,12 @@ describe('HomePage', () => {
     expect(screen.getAllByText("Discover & Share"))
     expect(screen.getAllByText("Generative AI-Prompts"))
     // expect(screen.getAllByText("PromptApp is an open-source AI prompting tool for modern world to discover, create and share creative prompt"))
+
+  })
+
+  it("should render the Feed Component", async () => {
+    render(<HomePage />)
+
+    expect(screen.getByTestId('feed-mock'))
   })
 })
