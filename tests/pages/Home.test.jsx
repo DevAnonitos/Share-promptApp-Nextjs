652fb5d42d5b0afbe0d10af84c4733429eb8a8a4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-13 23:22:46 +0700

    update:Add->TestConfig

--- tests/pages/Home.test.jsx
+++ tests/pages/Home.test.jsx
@@ -15,7 +15,6 @@ describe('HomePage', () => {
     expect(screen.getAllByText("Discover & Share"))
     expect(screen.getAllByText("Generative AI-Prompts"))
     // expect(screen.getAllByText("PromptApp is an open-source AI prompting tool for modern world to discover, create and share creative prompt"))
-
   })
 
   it("should render the Feed Component", async () => {
--- tests/pages/Layout.test.jsx
+++ tests/pages/Layout.test.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 describe("MainLayout", () => {
-  
+
 })
