8db631dfc699c1ec4a1d89154a3e1f26f59b38c2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-14 17:01:40 +0700

    update:Add->PromptCardTest

---
+++ tests/components/PromptCard.test.jsx
@@ -0,0 +1,7 @@
+import { PromptCard } from "@/components";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("PromptCardComponent", () => {
+  
+})
