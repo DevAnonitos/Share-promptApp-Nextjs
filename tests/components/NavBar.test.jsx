0f3313efb4eb803970465b46f56781a3d7380039
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-14 15:49:15 +0700

    update:Add->TestNavBar

---
+++ tests/components/NavBar.test.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("NavbarComponent", () => {
+  it("Should render title App", () => {
+    
+  })
+})
