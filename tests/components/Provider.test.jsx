c4afc2fcbd0165b13b16222d11b3f98f1a56af12
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-14 20:15:00 +0700

    update:Add->ProviderTestComponents

---
+++ tests/components/Provider.test.jsx
@@ -0,0 +1,7 @@
+import { Provider } from "@/components";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("ProviderComponent", () => {
+  
+})
