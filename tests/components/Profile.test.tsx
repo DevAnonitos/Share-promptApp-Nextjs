95efcf0efb701f2ac37a2960481181275ff88031
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-14 20:12:26 +0700

    update:Add->ProfileTest

---
+++ tests/components/Profile.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Profile } from "@/components";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("ProfileComponent", () => {
+  
+})
