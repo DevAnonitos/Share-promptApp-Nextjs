396467226257a439f2c9d01da3e922418a8b83ce
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-14 15:31:24 +0700

    update:Add->FormTest

---
+++ tests/components/Form.test.jsx
@@ -0,0 +1,10 @@
+import { Form } from "@/components";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("FormComponent", () => {
+  it("should render the title Form", () => {
+    // render(<Form />)
+    // expect(screen.getAllByPlaceholderText("Write Your Prompt Here..."))
+  })
+})
