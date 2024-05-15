c62a39e2f4d7897f51c95fe137471c062faa2a0d
2151013007bao <2151013007bao@ou.edu.vn>
2024-05-15 07:49:30 +0000

    update:Add->Axios

--- __tests__/pages/RootLayout.test.jsx
+++ __tests__/pages/RootLayout.test.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import RootLayout from "@/app/layout";
 import { render, screen } from "@testing-library/react";
+import RootLayout from "@/app/layout";
 
 describe("MainLayout", () => {
-    // it("should render the Navbarcomponent", () => {
-        render(<RootLayout />)
-    // })
+  it("should render the Navbar component", () => {
+    const children = <div>Test Children</div>; // Define the children JSX here or assign an appropriate value
 
-    it("should render the child content", () => {
-        
-    })
-})
+    render(<RootLayout
+        children={children} 
+    />);
+
+    const navbarElement = screen.getByTestId("navbar"); // Replace "navbar" with the test ID or selector of your Navbar component
+
+    expect(navbarElement).toBeInTheDocument();
+  });
+});
