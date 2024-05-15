2881af2ff47a427d2b2f39012db1331460da507c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-16 00:19:52 +0700

    update:Add->UpdatePrompt

--- __tests__/pages/UpdatePrompt.test.jsx
+++ __tests__/pages/UpdatePrompt.test.jsx
@@ -1,2 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import UpdatePrompt from "@/app/update-prompt/page";
+import React from "react";
+
+jest.mock('next-auth/react', () => ({
+  useSession: jest.fn(),
+}));
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  useSearchParams: jest.fn(),
+}));
+
+describe("UpdatePrompt", () => {
+  it("should update the prompt", () => {
+    
+  })
+})
