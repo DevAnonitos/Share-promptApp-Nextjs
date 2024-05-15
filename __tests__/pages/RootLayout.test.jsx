966a25276c127008d436789a2f36dd3b1de44bdc
2151013007bao <2151013007bao@ou.edu.vn>
2024-05-15 06:39:52 +0000

    update:Add->TestFolder

--- __tests__/pages/RootLayout.test.jsx
+++ __tests__/pages/RootLayout.test.jsx
@@ -3,5 +3,7 @@ import RootLayout from "@/app/layout";
 import { render, screen } from "@testing-library/react";
 
 describe("MainLayout", () => {
-    
+    it("should render the Navbarcomponent", () => {
+        
+    })
 })
