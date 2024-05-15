83c4b7ccedb934fedce7d0617675a12595fecbb6
2151013007bao <2151013007bao@ou.edu.vn>
2024-05-15 03:49:06 +0000

    update:Add->FixErrorLogicFeed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -46,7 +46,7 @@ const Feed = () => {
         const regex = new RegExp(searchtext, "i");
         return allPosts.filter(
             (item) =>
-                regex.test(item.creator.username) ||
+                regex.test(item.creator?.username) ||
                 regex.test(item.tag) ||
                 regex.test(item.prompt)
         );
