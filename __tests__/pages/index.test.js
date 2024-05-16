2abc44cf56129f8d07b11e5735478f64341b060c
2151013007bao <2151013007bao@ou.edu.vn>
2024-05-16 04:58:22 +0000

    update:Add->TestImportAllPages

---
+++ __tests__/pages/index.test.js
@@ -0,0 +1,15 @@
+import HomePage from "@/app/page";
+import CreatePrompt from "@/app/create-prompt/page";
+import MyProfile from "@/app/profile/page";
+import UpdatePrompt from "@/app/update-prompt/page";
+import UserProfile from "@/app/profile/[id]/page";
+
+describe('Page Imports', () => {
+    it('should import all Pages without errors', () => {
+        expect(HomePage).toBeDefined();
+        expect(CreatePrompt).toBeDefined();
+        expect(MyProfile).toBeDefined();
+        expect(UpdatePrompt).toBeDefined()
+        expect(UserProfile).toBeDefined()
+    });
+  });
