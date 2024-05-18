aed00c4f30aee798750409194ebb94f3b2ca3487
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-18 09:22:13 +0700

    update:Add->ConfigSomeTest

--- __tests__/api/prompt/route.test.js
+++ __tests__/api/prompt/route.test.js
@@ -2,13 +2,42 @@
 * @jest-environment node
 */
 import { GET } from "@/app/api/prompt/route";
+
+// jest.mock('@/libs/models/prompt'); 
+
+// export const mockDatabase = {
+//   connect: jest.fn().mockResolvedValueOnce(), // Mock successful connection
+//   find: jest.fn(), // Mock find method for queries
+// };
+
+// export const connectToDB = async () => {
+//   await mockDatabase.connect(); // Use mocked connection
+// };
+
 describe('API Prompt', () => {
   // Test fetch fail response all prompt
   it('should [GET]: a fail response data as JSON from API Route', async () => {
+    // mockDatabase.connect.mockResolvedValueOnce();
+
+    // // Mock Prompt.find to return expected data (replace with your structure)
+    // const mockFind = jest.fn().mockResolvedValueOnce([{ id: 1, name: "Prompt 1" }]);
+    // Prompt.find = mockFind; 
+
+    // const res = await GET();
+    // const data = await res.json();
+
+    // expect(res.status).toEqual(200); // Expect a successful response (200)
+    // expect(data).toBeDefined();
+    // expect(data.length).toBe(1); //
+    
+  });
+
+  it("should [GET]: a fail response data as JSON from API Route", async () => {
     const res = await GET();
     const data = await res.json();
     console.log(data);
-    expect(res.status).toEqual(500)
-    expect(data).toBeDefined()
-  });
+    expect(res.status).toEqual(500); 
+    expect(data).toBeDefined();
+  })
+
 });
