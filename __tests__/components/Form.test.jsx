a10afa848eb9f520afbcde6e4896f2acaca743c0
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-05-19 14:26:51 +0700

    update:Add->FormTest Components

--- __tests__/components/Form.test.jsx
+++ __tests__/components/Form.test.jsx
@@ -1,37 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Form from "../../components/Form"
+import { Form } from '@/components';
 
-jest.mock('../../components/Form', () => ({
-  __esModule: true,
-  default: jest.fn(),
-}));
-
-const mockPost = {
-  prompt: '',
-  tag: '',
-};
-
-const mockSubmitHandler = jest.fn();
-
-test('renders the Form component with placeholders', () => {
-  Form.mockImplementationOnce(() => (
-    <Form type="Create" post={mockPost} setPost={() => {}} submitting={false} handleSubmit={mockSubmitHandler} />
-  ));
-  render(<Form />); // You can render directly without props here
-
-  // Assertions using screen.getByText, screen.getByPlaceholderText, etc.
-  expect(screen.getByText(/Create Post/i)).toBeInTheDocument();
-  expect(screen.getByPlaceholderText(/Write Your Prompt Here.../i)).toBeInTheDocument();
-  expect(screen.getByText(/Field of Prompt/i)).toBeInTheDocument();
-});
-
-test('disables submit button when submitting is true', () => {
-  Form.mockImplementationOnce(() => (
-    <Form type="Create" post={mockPost} setPost={() => {}} submitting={true} handleSubmit={mockSubmitHandler} />
-  ));
-  render(<Form />);
-
-  const submitButton = screen.getByRole('button', { name: /Create/i });
-  expect(submitButton).toBeDisabled();
-});
+describe("Form Components", () => {
+    it("should render Form Component", () => {
+        expect(Form).toBeDefined()
+        expect(Form).toMatchSnapshot()
+    })
+})
