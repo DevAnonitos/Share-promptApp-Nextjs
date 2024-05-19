432b1fc6afff9dd1b4febd3c226aa7bf59ebf884
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-05-19 11:55:28 +0700

    update:Add->TestAuth [GET]

--- __tests__/api/auth/route.test.js
+++ __tests__/api/auth/route.test.js
@@ -1,26 +1,34 @@
 /**
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 
-import { GET } from "@/app/api/auth/[...nextauth]/route"
+import { GET } from "@/app/api/auth/[...nextauth]/route";
 
 describe("API Authentication", () => {
-  const baseUrl = "http://localhost:3000/api/auth/providers"
-  // Get API Auth from fetch
-  it("should return a data json from API auth routes", async () => {
-    const res = await fetch(baseUrl);
-    const data = await res.json();
-    console.log(data);
-    expect(data).toBeDefined();
-    expect(data).toMatchSnapshot()
-  })
+    const baseUrl = "http://localhost:3000/api/auth/providers";
+    // Get API Auth from fetch
+    it("should return a data json from API auth routes", async () => {
+        const res = await fetch(baseUrl);
+        const data = await res.json();
+        console.log(data);
+        expect(data).toBeDefined();
+        expect(data).toMatchSnapshot();
+    });
 
-  // a Status Code fetch API
-  it("should return a status 200 from API auth routes", async () => {
-    const res = await fetch(baseUrl);
-    const data = await res.json();
-    console.log(data);
-    expect(res.status).toBe(200);
-    expect(data).toMatchSnapshot()
-  })
-})
+    // a Status Code fetch API
+    it("should return a status 200 from API auth routes", async () => {
+        const res = await fetch(baseUrl);
+        const data = await res.json();
+        console.log(data);
+        expect(res.status).toBe(200);
+        expect(data).toMatchSnapshot();
+    });
+
+    it("should [GET] from get API Route /api/auth/providers", async () => {
+      const res = await GET()
+      const data = await res.json()
+      console.log(data)
+      expect(data).toBeDefined()
+      expect(data).toMatchSnapshot()
+    });
+});
