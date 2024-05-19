ae24e4ce9f543eeae654a180544623d6e9605f8d
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-05-19 13:09:45 +0700

    update:Add->Test ProfilePage

--- __tests__/pages/Profile.test.jsx
+++ __tests__/pages/Profile.test.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import MyProfile from "@/app/profile/page";
+import UserProfile from "@/app/profile/[id]/page";
 import { render, screen } from "@testing-library/react";
 
 describe("Profile Page", () => {
-  
+  it("should render MyProfile Page Component", () => {
+    expect(MyProfile).toBeDefined()
+    expect(MyProfile).toMatchSnapshot()
+  })
+
+  it("should render UserProfile Page Component", () => {
+    expect(UserProfile).toBeDefined()
+    expect(UserProfile).toMatchSnapshot()
+  })
 })
