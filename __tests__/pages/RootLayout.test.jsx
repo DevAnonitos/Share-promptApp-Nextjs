1719d4647ba28f7c77623ebcd5faad038e7ea64d
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-05-19 13:04:21 +0700

    update:Add->TestImportRootLayout

--- __tests__/pages/RootLayout.test.jsx
+++ __tests__/pages/RootLayout.test.jsx
@@ -3,7 +3,8 @@ import { render, screen } from "@testing-library/react";
 import RootLayout from "@/app/layout";
 
 describe("MainLayout", () => {
-  it("should render the Navbar component", () => {
-    
+  it("should render RootLayout component", () => {
+    expect(RootLayout).toBeDefined()
+    expect(RootLayout).toMatchSnapshot()
   });
 });
